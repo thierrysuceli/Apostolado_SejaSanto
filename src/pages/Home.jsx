407528a2180331c6363fc2b2e260718d11fd38d9
Thierrysuceli <thierrysueli2@gmail.com>
2025-12-22 11:33:50 -0300

    fix: Remove código duplicado em Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -806,13 +806,6 @@ const Home = () => {
           </div>
         </section>
       )}
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
-              </svg>
-            </Link>
-          </div>
-        </div>
-      </section>
 
       {/* Latest Articles Section */}
       <section className="py-16 px-4 sm:px-6 lg:px-8 bg-white dark:bg-gray-950">
